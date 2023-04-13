d070e51ec1a0bfa4a9567247c35629a33fb3d0dc
Vivek Vardhan Patchala <vpatchala@vpatcha-wsm4v8v.internal.salesforce.com>
2023-04-13 17:23:56 +0530

    initial commit

---
+++ src/main/java/com/largeapi/api/Largedata.java
@@ -0,0 +1,46 @@
+package com.largeapi.api; 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+@SpringBootApplication
+@RestController
+public class Largedata  {
+
+    @RequestMapping("/hello")
+    String hello() {
+        //return "Hello from MyApp!";
+        return pdfparser();
+    }
+
+    @RequestMapping("/echo")
+    String echo(@RequestParam("message") String message) {
+        return message;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Largedata.class, args);
+    }
+
+    private String pdfparser() {
+        try {
+        String folderPath = "~/Desktop/largedata/";
+        String fileName = "LargePdf.pdf";
+        File file = new File(folderPath + fileName);
+        
+        PDDocument document = PDDocument.load(file);
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        String text = pdfStripper.getText(document);
+        document.close();
+        return text;
+        }
+        catch(IOException e) {
+            return "Exception Occured"+e;
+        }
+    }
+
+}
