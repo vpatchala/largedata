3f22a40f89ebba422778780cf4d7c15ec7691f0a
Vivek Vardhan Patchala <vpatchala@vpatcha-wsm4v8v.internal.salesforce.com>
2023-04-13 17:58:48 +0530

    File changes 5

--- src/main/java/com/largeapi/api/Largedata.java
+++ src/main/java/com/largeapi/api/Largedata.java
@@ -31,7 +31,7 @@ public class Largedata  {
 
         StringBuilder sampleString = new StringBuilder();
         sampleString.append("a");
-        for ( int i =0;i<25;i++) {
+        for ( int i =0;i<20;i++) {
             sampleString.append(sampleString.toString());
         }
         return sampleString.toString();
